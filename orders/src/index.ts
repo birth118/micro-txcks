212d8b4e0066cf0eb46961920f95d04c869b5850
Seongsoo Yim <seongsooyim@192-168-1-100.tpgi.com.au>
2020-12-31 17:09:49 +1100

    add startup message in orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -11,6 +11,8 @@ import { Ticket } from './models/ticket'
 const port = process.env.PORT || 3000
 
 const start = async () => {
+  console.log('Starting...')
+
   //   DB_TEST_URL=mongodb://127.0.0.1:27017
 
   if (!process.env.JWT_KEY) {
